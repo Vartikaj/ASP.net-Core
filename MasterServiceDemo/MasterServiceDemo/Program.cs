fcf3a4a8e193e0cff5b0993c790a1213ca5ba919
Vartika <vartikajohari@cdac.in>
2025-01-22 09:36:12 +0530

    First commit

---
+++ MasterServiceDemo/MasterServiceDemo/Program.cs
@@ -0,0 +1,30 @@
+using CommonService.Utility;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add Swagger Custom Documentation with Authorize the header functionality
+builder.Services.AddSwaggerDocumentation();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
